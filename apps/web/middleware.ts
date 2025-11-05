6f25c7b360f97652b9e99aedae31dfc6e5f1267f
Vipul Yadav <devs.vipul@gmail.com>
2025-11-06 04:00:43 +0530

    feat: complete auth with clerk

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -17,7 +17,6 @@ export const config = {
   matcher: [
     // Skip Next.js internals and all static files
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-
     "/(api|trpc)(.*)",
   ],
 };
